f6a2b7254c017ad662862b42e09b34d2d0eb260a
mahamaharathi <71564898+mahamaharathi@users.noreply.github.com>
2025-02-04 23:41:34 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx}"], // Include index.html
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
